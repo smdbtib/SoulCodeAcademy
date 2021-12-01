5978868a5a11740d9ea0216fcb82c9095a2e6855
smdbtib <smdb.tib@outlook.com>
2021-12-01 11:13:05 -0300

    Aula 01 JS

---
+++ Aulas_JS/Aula_01/scripts/main.js
@@ -0,0 +1,30 @@
+var valor1 = 10;
+var valor2 = 15;
+var valor3 = 9;
+var valor4 = 7;
+var valor5 = "10";
+
+
+//&&
+if ((valor1 < 5 )&&(valor2 > 5)){
+    console.log("Deu bom!");
+}else{
+    console.log("Deu bom não!");
+}
+
+//||
+
+if ((valor1 > 5 ) || (Valor2 > 5)){
+    console.log("Deu bom!");
+}else{
+    console.log("Não deu bom!")
+}
+
+//USO DO PROMPT
+var info = prompt("Digite um número interiro");
+
+if (info > 10) {
+    alert("O número digitado é maior do que 10.")
+}else{
+    alert("O número digitado não é maior do que 10.")
+}
